31961e227ef13ccf7a0b0c487bdfc58f3a2cbe39
pubupka <Halimovm111@gmail.com>
2023-10-27 21:12:02 +0600

    add MoveCommand, Add methods to Vector, build

---
+++ spacebattle/SpaceBattle.Lib/MoveCommand.cs
@@ -0,0 +1,13 @@
+public class MoveCommand : ICommand
+{
+    IMovable movable;
+    public MoveCommand(IMovable movable)
+    {
+        this.movable = movable;
+    }
+
+    public void Execute()
+    {
+        movable.Position += movable.Velocity;
+    }
+}
--- spacebattle/SpaceBattle.Lib/Vector.cs
+++ spacebattle/SpaceBattle.Lib/Vector.cs
@@ -10,9 +10,41 @@ public class Vector
     {
         int[] array = new int[v1.array.Length];
 
-        for (int i=0; i<array.Length; i++)
+        for (int i=0; i < array.Length; i++)
             array[i] = v1.array[i] + v2.array[i];
 
         return new Vector(array);
+    }
+
+    // == сравнивает ссылки, а equals объекты
+    public override bool Equals(object? obj)
+    {
+        
+        if (obj == null || this.GetType() != obj.GetType())
+        {
+            return false;
+        }
+        
+        return base.Equals(obj);
+    }
+    
+    public override int GetHashCode()
+    {
+        return this.array.GetHashCode();
+    }
+
+    public static bool operator==(Vector v1, Vector v2)
+    {
+        for (int i=0; i < v1.array.Length; i++)
+        {
+            if (v1.array[i] != v2.array[i])
+                return false;
+        }
+        return true;
+    }
+
+    public static bool operator!=(Vector v1, Vector v2)
+    {
+        return !(v1 == v2);
     }
 }
