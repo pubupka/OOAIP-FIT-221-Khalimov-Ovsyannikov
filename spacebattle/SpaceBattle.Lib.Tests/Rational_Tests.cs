78711a385ffbbcba416dbc6d139c223fa60cd19a
arte645 <artm.ovs@mail.ru>
2023-11-04 12:54:59 +0000

    add denomirator to Rational, add tests for it, add .gitignore, changed Turn.yml

--- spacebattle/SpaceBattle.Lib.Tests/Rational_Tests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/Rational_Tests.cs
@@ -17,11 +17,49 @@ namespace SpaceBattle.Lib.Tests
         }
 
         [Fact]
-        public void Rational_Unequals()
+        public void RationalUnequals()
         {
-            Rational angle = new Rational(45);
+            Rational angle = new Rational(45, -360);
             Rational delta = new Rational(90);
             Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalUnequalsNumerator()
+        {
+            Rational angle = new Rational(13,360);
+            Rational delta = new Rational(17);
+            Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalUnequalsNumeratorAndDenomirator()
+        {
+            Rational angle = new Rational(1,2);
+            Rational delta = new Rational(2,3);
+            Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalUnequalsDenomirator()
+        {
+            Rational angle = new Rational(1,2);
+            Rational delta = new Rational(1,6);
+            Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalEqualsNumeratorAndDenomiratorWrong()
+        {
+            Rational angle = new Rational(1,2);
+            Rational delta = new Rational(2,3);
+            Assert.False(angle == delta);
+        }
+
+        [Fact]
+        public void RationalDenominatorIsZero()
+        {
+           Assert.Throws<Exception>(() => new Rational(17,0));
         }
 
         [Fact]
@@ -31,11 +69,20 @@ namespace SpaceBattle.Lib.Tests
             Rational delta = new Rational(45);
             Assert.Equal(angle.GetHashCode(), delta.GetHashCode());
         }
+
         [Fact]
         public void Rational_Equals_Null()
         {
             Rational angle = new Rational(45);
             Assert.False(angle.Equals(null));
+        }
+
+        [Fact]
+        public void RationalEqualsFalseNumerator()
+        {
+            Rational angle = new Rational(45);
+            Rational delta = new Rational(45,360);
+            Assert.True(angle.Equals(delta));
         }
     }
 }
--- spacebattle/SpaceBattle.Lib/Rational.cs
+++ spacebattle/SpaceBattle.Lib/Rational.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Linq.Enumerable;
 using System.Threading.Tasks;
 
 namespace SpaceBattle
@@ -8,16 +9,59 @@ namespace SpaceBattle
     public class Rational
     {
         private int numerator;
-        private int denominator = 360;
+        private int denominator;
 
-        public Rational(int num)
+        public Rational(int num, int denominator=360)
         {
             this.numerator = num % 360;
+
+            this.DevideByZero(denominator);
+            this.denominator = denominator;
+
+            this.Sign();
+            this.Minimize();
+        }
+
+         private void DevideByZero(int input_denum)
+        {
+            try
+            {
+                var test = this.numerator / input_denum;
+            }
+            catch (DivideByZeroException)
+            {
+                throw new Exception();
+            }
+        }
+
+         private void Sign()
+        {
+            if (this.numerator * this.denominator < 0)
+            {
+                this.numerator = Math.Abs(this.numerator) * (-1);
+                this.denominator = Math.Abs(this.denominator);
+            }
+            else
+            {
+                this.numerator = Math.Abs(this.numerator);
+                this.denominator = Math.Abs(this.denominator);
+            }
+        }
+
+         public void Minimize()
+        {
+
+            var od = Range(1, Math.Min(Math.Abs(this.numerator), Math.Abs(this.denominator))).Where(x => this.denominator % x == 0 && 
+                                                                            this.numerator % x == 0).Select(x => x);
+            int nod = od.Last();
+
+            this.numerator /= nod;
+            this.denominator /= nod;
         }
 
         public override bool Equals(object? obj)
         {
-            return obj is Rational r && this.numerator == r.numerator;
+            return obj is Rational r && this.numerator == r.numerator && this.denominator == r.denominator;
         }                                           
         
         public override int GetHashCode() => HashCode.Combine(numerator, denominator);
@@ -25,17 +69,18 @@ namespace SpaceBattle
         public static Rational operator+(Rational angle, Rational delta)
         {
             angle.numerator = (angle.numerator + delta.numerator) % angle.denominator;
+            angle.Minimize();
             return angle;
         }
 
         public static bool operator==(Rational angle, Rational delta)
         {
-            return angle.numerator==delta.numerator;
+            return angle.numerator==delta.numerator && angle.denominator==delta.denominator;
         }
 
         public static bool operator!=(Rational angle, Rational delta)
         {
-            return angle.numerator!=delta.numerator;
+            return angle.numerator!=delta.numerator || angle.denominator!=delta.denominator;
         }
     }
 }
