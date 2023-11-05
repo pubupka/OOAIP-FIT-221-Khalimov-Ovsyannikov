a40efccc5f8792064d6a200bac6447c651f9414d
arte645 <artm.ovs@mail.ru>
2023-11-05 06:55:43 +0000

    changed operation "+" for Rational, add "_" in variables` names

--- spacebattle/SpaceBattle.Lib/Rational.cs
+++ spacebattle/SpaceBattle.Lib/Rational.cs
@@ -8,15 +8,15 @@ namespace SpaceBattle
 {
     public class Rational
     {
-        private int numerator;
-        private int denominator;
+        private int _numerator;
+        private int _denominator;
 
         public Rational(int num, int denominator=360)
         {
-            this.numerator = num % 360;
+            this._numerator = num % 360;
 
             this.DevideByZero(denominator);
-            this.denominator = denominator;
+            this._denominator = denominator;
 
             this.Sign();
             this.Minimize();
@@ -26,7 +26,7 @@ namespace SpaceBattle
         {
             try
             {
-                var test = this.numerator / input_denum;
+                var test = this._numerator / input_denum;
             }
             catch (DivideByZeroException)
             {
@@ -36,51 +36,53 @@ namespace SpaceBattle
 
          private void Sign()
         {
-            if (this.numerator * this.denominator < 0)
+            if (this._numerator * this._denominator < 0)
             {
-                this.numerator = Math.Abs(this.numerator) * (-1);
-                this.denominator = Math.Abs(this.denominator);
+                this._numerator = Math.Abs(this._numerator) * (-1);
+                this._denominator = Math.Abs(this._denominator);
             }
             else
             {
-                this.numerator = Math.Abs(this.numerator);
-                this.denominator = Math.Abs(this.denominator);
+                this._numerator = Math.Abs(this._numerator);
+                this._denominator = Math.Abs(this._denominator);
             }
         }
 
          public void Minimize()
         {
 
-            var od = Range(1, Math.Min(Math.Abs(this.numerator), Math.Abs(this.denominator))).Where(x => this.denominator % x == 0 && 
-                                                                            this.numerator % x == 0).Select(x => x);
+            var od = Range(1, Math.Min(Math.Abs(this._numerator), Math.Abs(this._denominator))).Where(x => this._denominator % x == 0 && 
+                                                                            this._numerator % x == 0).Select(x => x);
             int nod = od.Last();
 
-            this.numerator /= nod;
-            this.denominator /= nod;
+            this._numerator /= nod;
+            this._denominator /= nod;
         }
 
         public override bool Equals(object? obj)
         {
-            return obj is Rational r && this.numerator == r.numerator && this.denominator == r.denominator;
+            return obj is Rational r && this._numerator == r._numerator && this._denominator == r._denominator;
         }                                           
         
-        public override int GetHashCode() => HashCode.Combine(numerator, denominator);
+        public override int GetHashCode() => HashCode.Combine(_numerator, _denominator);
 
         public static Rational operator+(Rational angle, Rational delta)
         {
-            angle.numerator = (angle.numerator + delta.numerator) % angle.denominator;
+            angle._numerator = (angle._numerator*delta._denominator + delta._numerator*angle._denominator);
+            angle._denominator *= delta._denominator; 
             angle.Minimize();
+            angle._numerator = angle._numerator % angle._denominator;
             return angle;
         }
 
         public static bool operator==(Rational angle, Rational delta)
         {
-            return angle.numerator==delta.numerator && angle.denominator==delta.denominator;
+            return angle._numerator==delta._numerator && angle._denominator==delta._denominator;
         }
 
         public static bool operator!=(Rational angle, Rational delta)
         {
-            return angle.numerator!=delta.numerator || angle.denominator!=delta.denominator;
+            return angle._numerator!=delta._numerator || angle._denominator!=delta._denominator;
         }
     }
 }
