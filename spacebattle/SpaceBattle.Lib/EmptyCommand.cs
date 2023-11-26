d2e9b782e77678f17b3cf47a39fca3372e3609ba
arte645 <artm.ovs@mail.ru>
2023-11-26 14:32:31 +0000

    add EmptyCommand

---
+++ spacebattle/SpaceBattle.Lib/EmptyCommand.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceBattle.Lib
+{
+    public class EmptyCommand : ICommand
+    {
+        public void Execute();
+    }
+}
