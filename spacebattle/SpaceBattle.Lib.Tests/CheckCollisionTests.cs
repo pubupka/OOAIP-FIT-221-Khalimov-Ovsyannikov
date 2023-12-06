ea6ac04378fb660e36f75161a152cdb31c1418d5
arte645 <artm.ovs@mail.ru>
2023-12-06 16:57:44 +0000

    removed useless transformation in tests

--- spacebattle/SpaceBattle.Lib.Tests/CheckCollisionTests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/CheckCollisionTests.cs
@@ -19,7 +19,7 @@ namespace SpaceBattle.Lib.Tests
                 var positionSecond = (List<int>)args[2];
                 var velocitySecond = (List<int>)args[3];
 
-                var variation = positionFirst.Select((value, index) => value - positionSecond[index]).Concat(velocityFirst.Select((value, index) => value - velocitySecond[index]).ToList()).ToList();
+                var variation = positionFirst.Select((value, index) => value - positionSecond[index]).Concat(velocityFirst.Select((value, index) => value - velocitySecond[index])).ToList();
 
                 return variation;
             }).Execute();
