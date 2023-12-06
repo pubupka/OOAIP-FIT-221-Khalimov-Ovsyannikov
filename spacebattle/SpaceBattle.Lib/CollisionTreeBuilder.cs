db8f151b8a523c75de59e579583e7435474cec71
pubupka <Halimovm111@gmail.com>
2023-12-06 21:47:41 +0600

    some fixes

--- spacebattle/SpaceBattle.Lib/CollisionTreeBuilder.cs
+++ spacebattle/SpaceBattle.Lib/CollisionTreeBuilder.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using Hwdtech;
 
-public class CollisionTreeBuilder
+public class BuildCollisionTreeCommand: ICommand
 {
     private readonly List<int[]> _arrays;
 
-    public CollisionTreeBuilder(string path)
+    public BuildCollisionTreeCommand(string path)
     {
         _arrays = IoC.Resolve<CollisionTextFile_WithSpaceAsSeparator_Reader>("Game.Collisions.TreeBuilder", path).ReadArrays();
+    }
 
+    public void Execute()
+    {
         _arrays.ForEach(array => {
             var node = IoC.Resolve<Hashtable>("Game.Collisions.Tree");
             array.ToList().ForEach(num => {
-                if (!node.ContainsKey(num))
-                {
-                    node.Add(num, new Hashtable());
-                }
-                node = (Hashtable)node[num];
+                _ = node.ContainsKey(num) ? node[num] : node[num] = new Hashtable();
+                // if (!node.ContainsKey(num))
+                // {
+                //     node.Add(num, new Hashtable());
+                // }
+
+                node = (Hashtable)node[num]!;
             });
         });
     }
