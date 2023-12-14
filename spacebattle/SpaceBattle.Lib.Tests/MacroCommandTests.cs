1ca98aa70fa42196f6b6bc45e7ba030daed1483d
pubupka <Halimovm111@gmail.com>
2023-12-14 19:00:48 +0600

    add negative test (atomaric command throws exception), part of code in tests moved into ctor

--- spacebattle/SpaceBattle.Lib.Tests/MacroCommandTests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/MacroCommandTests.cs
@@ -6,6 +6,11 @@ public class MacroCommandTests
     public MacroCommandTests()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.MacroCommands.MoveWithCheckFuel", (object[] args) => {
+            return new string[] { "Game.Commands.Move", "Game.Commands.CheckFuel" };
+        }).Execute();
     }
 
     [Fact]
@@ -23,10 +28,6 @@ public class MacroCommandTests
             return checkFuelCommand.Object;
         }).Execute();
 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.MacroCommands.MoveWithCheckFuel", (object[] args) => {
-            return new string[] { "Game.Commands.Move", "Game.Commands.CheckFuel" };
-        }).Execute();
-
         var macroCommand = new MacroCommand("Game.MacroCommands.MoveWithCheckFuel");
         macroCommand.Execute();
 
@@ -35,4 +36,25 @@ public class MacroCommandTests
     }
 
     // Тест 2: Одна или несколько команд выкидывают исключение при выполнении
+    // Тест 3: В макрокоманду передаётся имя несуществующей зависимости
+
+    [Fact]
+    public void OneOfAtomaricCommandsThrowsException()
+    {
+        var moveCommand = new Mock<ICommand>();
+        moveCommand.Setup(mc => mc.Execute()).Throws(new NotImplementedException());
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.Move", (object[] args) => {
+            return moveCommand.Object;
+        }).Execute();
+
+        var checkFuelCommand = new Mock<ICommand>();
+        checkFuelCommand.Setup(cfc => cfc.Execute()).Callback(() => {});
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.CheckFuel", (object[] args) => {
+            return checkFuelCommand.Object;
+        }).Execute();
+
+        var macroCommand = new MacroCommand("Game.MacroCommands.MoveWithCheckFuel");
+
+        Assert.Throws<NotImplementedException>(macroCommand.Execute);
+    }
 }
