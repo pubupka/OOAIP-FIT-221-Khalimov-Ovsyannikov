79cbe93d7804a1b5e29d3abc1d01fb55913abb95
Bup <113292898+arte645@users.noreply.github.com>
2023-12-15 17:25:02 +0600

    Lab3 check collision ovsyannikov (#6)
    * add CheckCollisionCommand
    
    * add tests
    
    * add tests(really)
    
    * removed useless transformation in tests
    
    * add InitFindVariationsCommand
    
    * changed format and add empty line
    
    ---------
    
    Co-authored-by: arte645 <artm.ovs@mail.ru>

---
+++ spacebattle/SpaceBattle.Lib/CheckCollisionCommand.cs
@@ -0,0 +1,31 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib
+{
+    public class CheckCollisionCommand : ICommand
+    {
+        private readonly IUObject _objFirst, _objSecond;
+
+        public CheckCollisionCommand(IUObject objFirst, IUObject objSecond)
+        {
+            _objFirst = objFirst;
+            _objSecond = objSecond;
+        }
+
+        public void Execute()
+        {
+            var positionFirst = IoC.Resolve<List<int>>("Game.UObject.GetProperty", _objFirst, "Position");
+            var velocityFirst = IoC.Resolve<List<int>>("Game.UObject.GetProperty", _objFirst, "Velocity");
+            var positionSecond = IoC.Resolve<List<int>>("Game.UObject.GetProperty", _objSecond, "Position");
+            var velocitySecond = IoC.Resolve<List<int>>("Game.UObject.GetProperty", _objSecond, "Velocity");
+
+            var variations = IoC.Resolve<List<int>>("Game.UObject.FindVariations", positionFirst, positionSecond, velocityFirst, velocitySecond);
+
+            var collisionTree = IoC.Resolve<IDictionary<int, object>>("Game.Command.BuildCollisionTree");
+
+            variations.ForEach(variation => collisionTree = (IDictionary<int, object>)collisionTree[variation]);
+
+            IoC.Resolve<ICommand>("Game.Event.Collision", _objFirst, _objSecond).Execute();
+        }
+    }
+}
