a88c70255c95a4d6c83cd9376e23fd0547e881d2
arte645 <artm.ovs@mail.ru>
2023-12-15 08:05:25 +0000

    add InitFindVariationsCommand

--- spacebattle/SpaceBattle.Lib.Tests/CheckCollisionTests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/CheckCollisionTests.cs
@@ -12,17 +12,7 @@ namespace SpaceBattle.Lib.Tests
 
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.GetProperty", (object[] args) => new List<int> { 1, 1, 1, 1, 1 }).Execute();
 
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.FindVariations", (object[] args) =>
-            {
-                var positionFirst = (List<int>)args[0];
-                var velocityFirst = (List<int>)args[1];
-                var positionSecond = (List<int>)args[2];
-                var velocitySecond = (List<int>)args[3];
-
-                var variation = positionFirst.Select((value, index) => value - positionSecond[index]).Concat(velocityFirst.Select((value, index) => value - velocitySecond[index])).ToList();
-
-                return variation;
-            }).Execute();
+            new InitFindVariationsCommand().Execute();
 
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.CheckCollision", (object[] args) => new CheckCollisionCommand((IUObject)args[0], (IUObject)args[1])).Execute();
         }
