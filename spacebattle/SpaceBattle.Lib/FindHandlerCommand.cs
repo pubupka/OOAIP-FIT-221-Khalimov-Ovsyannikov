848550496b2c3ad0e6e68f65a41c639cadda72de
pubupka <Halimovm111@gmail.com>
2023-12-26 21:12:34 +0600

    fix FindHandlerCommand

--- spacebattle/SpaceBattle.Lib/FindHandlerCommand.cs
+++ spacebattle/SpaceBattle.Lib/FindHandlerCommand.cs
@@ -3,22 +3,30 @@ using Hwdtech;
 
 public class FindHandlerCommand : ICommand
 {
-    private readonly Dictionary<string, Dictionary<string, IHandler>> _handleTree;
-    private readonly string _cmdName;
-    private readonly string _exName;
+    private readonly Dictionary<Type, Dictionary<Type, IHandler>> _handleTree;
+    private readonly Type _cmdType;
+    private readonly Type _exType;
 
-    public FindHandlerCommand(string cmdName, string exName)
+    public FindHandlerCommand(Type cmdType, Type exType)
     {
-        _handleTree = IoC.Resolve<Dictionary<string, Dictionary<string, IHandler>>>("Game.GetHandleTree");
-        _cmdName = cmdName;
-        _exName = exName;
+        _handleTree = IoC.Resolve<Dictionary<Type, Dictionary<Type, IHandler>>>("Game.GetHandleTree");
+        _cmdType = cmdType;
+        _exType = exType;
     }
 
     public void Execute()
     {
-        if (_handleTree.TryGetValue(_cmdName, out var dict) && dict.TryGetValue(_exName, out var handler))
-        {
-            IoC.Resolve<ICommand>("Game.HandleExceptionCommand", _cmdName, handler).Execute();
-        }
+        var subtree = _handleTree.GetValueOrDefault(_cmdType, new Dictionary<Type, IHandler>() {
+            { typeof(Exception), IoC.Resolve<IHandler>("Game.DefaultHandler") }
+        });
+
+        var handler = subtree.GetValueOrDefault(_exType, IoC.Resolve<IHandler>("Game.DefaultHandler"));
+
+        IoC.Resolve<ICommand>("Game.HandleExceptionCommand", _cmdType, handler);
+
+        // if (_handleTree.TryGetValue(_cmdName, out var dict) && dict.TryGetValue(_exName, out var handler))
+        // {
+        //     IoC.Resolve<ICommand>("Game.HandleExceptionCommand", _cmdName, handler).Execute();
+        // }
     }
 }
