348d97d05a892097ad4ac3af22604b808953b0ca
arte645 <artm.ovs@mail.ru>
2024-04-23 04:35:32 +0000

    rll add setting prop file

---
+++ spacebattle/SpaceBattle.Lib/SettingPropertiesAndReturningCommandByMessageStrategy.cs
@@ -0,0 +1,19 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib
+{
+    public class SettingPropertiesAndReturningCommandByMessageStrategy : IStrategy
+    {
+        public object Invoke(object[] args)
+        {
+            var message = (IProcessable)args[0];
+            var gameId = message.gameId;
+            var itemId = message.gameItemId;
+            var attributes = message.attributes;
+            var gameItemsdict = IoC.Resolve<Dictionary<string, Dictionary<int, IUObject>>>("Server.GameItemsdict");
+            var item = gameItemsdict[gameId][itemId];
+            attributes.ToList().ForEach(atr => item.SetProperty(atr.Key, atr.Value));
+            return IoC.Resolve<ICommand>("Game.Command." + message.cmdType, item);
+        }
+    }
+}
