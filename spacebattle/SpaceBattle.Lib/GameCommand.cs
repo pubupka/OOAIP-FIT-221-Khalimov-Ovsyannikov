264ae9d8de4580fd16c12535c42be4c63d26dbe7
pubupka <Halimovm111@gmail.com>
2024-05-01 16:39:29 +0600

    feat: add GameCommand

---
+++ spacebattle/SpaceBattle.Lib/GameCommand.cs
@@ -0,0 +1,45 @@
+using Hwdtech;
+using System.Diagnostics;
+
+namespace SpaceBattle.Lib
+{
+    public class GameCommand : ICommand
+    {
+        private readonly Queue<ICommand> _q;
+        private readonly object _scope;
+        private readonly Stopwatch _sw;
+        public GameCommand(Queue<ICommand> q, object scope)
+        {
+            _q = q;
+            _scope = scope;
+            _sw = new Stopwatch();
+        }
+
+        public void Execute()
+        {
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", _scope).Execute();
+
+            while(_sw.ElapsedMilliseconds < (int)IoC.Resolve<object>("Game.GetQuant"))
+            {
+                if (_q.Count == 0)
+                {
+                    break;
+                }
+
+                _sw.Start();
+                var cmd = _q.Dequeue();
+
+                try
+                {
+                    cmd.Execute();
+                }
+                catch (Exception e)
+                {
+                    IoC.Resolve<ICommand>("Game.Exception.Handle", cmd, e).Execute();
+                }
+
+                _sw.Stop();
+            }
+        }
+    }
+}
