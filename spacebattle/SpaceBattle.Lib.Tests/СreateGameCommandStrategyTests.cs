aa5022da513e98fdbb6f523d5f6ab278cb300e76
arte645 <artm.ovs@mail.ru>
2024-05-04 14:00:17 +0000

    fixed type of input prop of macrocommand

--- spacebattle/SpaceBattle.Lib.Tests/СreateGameCommandStrategyTests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/СreateGameCommandStrategyTests.cs
@@ -14,7 +14,6 @@ namespace SpaceBattle.Lib.Tests
         {
             new InitScopeBasedIoCImplementationCommand().Execute();
             IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.CreateGameCommandStrategy", (object[] args) => new CreateGameCommandStrategy().Invoke(args)).Execute();
         }
 
         [Fact]
@@ -22,27 +21,27 @@ namespace SpaceBattle.Lib.Tests
         {
             var mockCmd = new Mock<ICommand>();
             mockCmd.Setup(x => x.Execute()).Verifiable();
-            var injectCmd = new InjectCommand(mockCmd.Object);
 
             var gameDict = new Dictionary<string, ICommand>();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.CreateGameCommandStrategy", (object[] args) => new CreateGameCommandStrategy().Invoke(args)).Execute();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Dict", (object[] args)=> gameDict).Execute();
 
             var threadCollection = new BlockingCollection<ICommand>();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Server.Thread.Games", (object[] args)=> threadCollection).Execute();
 
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Scope.New", (object[] args) => (object)"bubki").Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Scope.New", (object[] args) => (object)0).Execute();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.New", (object[] args)=> new Queue<ICommand>()).Execute();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command", (object[] args) => mockCmd.Object).Execute();
 
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.Inject",(object[] args) => injectCmd).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.Inject",(object[] args) => new InjectCommand((ICommand)args[0])).Execute();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Command.Concurrent.Repeat", (object[] args)=> new RepeatConcurrentCommand((ICommand)args[0])).Execute();
             IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.Macro",(object[] args)=>{
                 var cmds = (List<ICommand>)args[0];
-                return new MacroCommand(cmds.ToArray());
+                return new MacroComand(cmds);
                 }).Execute();
             
             var gameCmd = IoC.Resolve<ICommand>("Game.CreateGameCommandStrategy", "asdfg", IoC.Resolve<object>("Scopes.Current"), 5);
-            Assert.Equal(injectCmd, gameDict["asdfg"]);
+            Assert.Equal(gameCmd, gameDict["asdfg"]);
 
             mockCmd.Verify(x=>x.Execute(), Times.Never());
             Assert.Empty(threadCollection);
