e27ceb5fbded34b8016a0ea54bb22c0cca6e8486
pubupka <Halimovm111@gmail.com>
2024-05-31 00:39:12 +0600

    feat: add SetFuelToSpaceshipsCommand

---
+++ spacebattle/SpaceBattle.Lib/SetFuelToSpaceshipsCommand.cs
@@ -0,0 +1,28 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib
+{
+    public class SetFuelToSpaceshipsCommand : ICommand
+    {
+        private readonly List<IUObject> _uObjects;
+        private readonly List<int> _fuelCapacities;
+        public SetFuelToSpaceshipsCommand(List<IUObject> uObjects, List<int> fuelCapacities)
+        {
+            _uObjects = uObjects;
+            _fuelCapacities = fuelCapacities;
+        }
+
+        public void Execute()
+        {
+            IEnumerator<int> fuelCapacitiesEnumerator = _fuelCapacities.GetEnumerator();
+
+            _uObjects.ForEach(u =>
+            {
+                IoC.Resolve<ICommand>("Game.IUObject.SetProperty", u, "FuelCapacity", fuelCapacitiesEnumerator.Current);
+                fuelCapacitiesEnumerator.MoveNext();
+            });
+
+            fuelCapacitiesEnumerator.Reset();
+        }
+    }
+}
