f5d159ece6b1395b2f4d7a7fcc50ec6c235ee953
pubupka <Halimovm111@gmail.com>
2024-05-31 00:05:24 +0600

    feat: add InitSpaceshipsCommand

---
+++ spacebattle/SpaceBattle.Lib/InitSpaceshipsCommand.cs
@@ -0,0 +1,28 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib
+{
+    public class InitSpaceshipsCommand : ICommand
+    {
+        private readonly List<IUObject> _uObjects;
+        private readonly List<Vector> _positions;
+        public InitSpaceshipsCommand(List<IUObject> uObjects, List<Vector> positions)
+        {
+            _uObjects = uObjects;
+            _positions = positions;
+        }
+
+        public void Execute()
+        {
+            System.Collections.IEnumerator positionsEnumerator = _positions.GetEnumerator();
+
+            _uObjects.ForEach(u =>
+            {
+                IoC.Resolve<ICommand>("Game.IUObject.SetProperty", u, "Position", positionsEnumerator.Current);
+                positionsEnumerator.MoveNext();
+            });
+
+            positionsEnumerator.Reset();
+        }
+    }
+}
